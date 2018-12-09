c3234321677d42a8967814cf1cd0785fd36728b8
Thomas Toye <thomas@toye.io>
2018-12-09 18:53:06 +0100

    Expand tenant type

--- src/tenant.ts
+++ src/tenant.ts
@@ -1,4 +1,30 @@
 export interface Tenant {
     id?: string;
     name: string;
+
+    /**
+     * General email address for this tenant
+     *
+     * E.g. info@organisation.org
+     */
+    email?: string;
+
+    /**
+     * Mail address for this organisation - not necessarily the address of the organisation itself
+     */
+    address: {
+        streetAndNumber?: string;
+        zipCode?: string;
+        city?: string;
+    };
+
+    /**
+     * The contact person for this organisation
+     */
+    contactPerson: {
+        name?: string;
+        phone?: string;
+        email?: string;
+    }
+
 }
