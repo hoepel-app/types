99ddab8062ebf36aacf79d3f556740522ac7d646
Thomas Toye <thomas@toye.io>
2019-06-04 20:06:26 +0200

    Add Discount test

---
+++ tests/discount.test.ts
@@ -0,0 +1,12 @@
+import { expect } from "chai";
+import { Discount } from "../src/discount";
+
+describe("Discount", () => {
+    it("Relative discount must be e [0, 100]", () => {
+        expect(() => new Discount({ name: 'test', relativeDiscount: -1 })).to.throw();
+        expect(() => new Discount({ name: 'test', relativeDiscount: 101 })).to.throw();
+        expect(() => new Discount({ name: 'test', relativeDiscount: 0 })).not.to.throw();
+        expect(() => new Discount({ name: 'test', relativeDiscount: 100 })).not.to.throw();
+        expect(() => new Discount({ name: 'test', relativeDiscount: 50 })).not.to.throw();
+    });
+});
