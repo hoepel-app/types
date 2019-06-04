c43bc3d78fb5c2d94d77df3c3b57798767e48e93
Thomas Toye <thomas@toye.io>
2019-06-04 20:09:35 +0200

    Fix lint errors

--- tests/discount.test.ts
+++ tests/discount.test.ts
@@ -3,10 +3,10 @@ import { Discount } from "../src/discount";
 
 describe("Discount", () => {
     it("Relative discount must be e [0, 100]", () => {
-        expect(() => new Discount({ name: 'test', relativeDiscount: -1 })).to.throw();
-        expect(() => new Discount({ name: 'test', relativeDiscount: 101 })).to.throw();
-        expect(() => new Discount({ name: 'test', relativeDiscount: 0 })).not.to.throw();
-        expect(() => new Discount({ name: 'test', relativeDiscount: 100 })).not.to.throw();
-        expect(() => new Discount({ name: 'test', relativeDiscount: 50 })).not.to.throw();
+        expect(() => new Discount({ name: "test", relativeDiscount: -1 })).to.throw();
+        expect(() => new Discount({ name: "test", relativeDiscount: 101 })).to.throw();
+        expect(() => new Discount({ name: "test", relativeDiscount: 0 })).not.to.throw();
+        expect(() => new Discount({ name: "test", relativeDiscount: 100 })).not.to.throw();
+        expect(() => new Discount({ name: "test", relativeDiscount: 50 })).not.to.throw();
     });
 });
