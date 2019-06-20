6fabb4a7615e277a02f4f8528c13e209cd40423b
Thomas Toye <thomas@toye.io>
2019-06-20 15:28:34 +0200

    Remove unused repository interfaces

--- src/repositories/repositories.ts
+++ src/repositories/repositories.ts
@@ -2,30 +2,6 @@
 //      just to type an interface)
 
 /**
- * Repository for a collection that returns the id with the object
- */
-export interface WithIdRepository<T> {
-    get(id: string): Promise<T>;
-    getAll(): Promise<ReadonlyArray<{ readonly obj: T, readonly id: string }>>;
-    getMany(ids: ReadonlyArray<string>): Promise<ReadonlyArray<{ readonly obj: T, readonly id: string }>>;
-    delete(id: string): Promise<void>;
-    // TODO create
-    // TODO update
-}
-
-/**
- * Repository for a collection indexed by tenant that returns the id with the object
- */
-export interface TenantIndexedWithIdRepository<T> {
-    get(tenant: string, id: string): Promise<T>;
-    getAll(tenant: string): Promise<ReadonlyArray<{ readonly obj: T, readonly id: string }>>;
-    getMany(tenant: string, ids: ReadonlyArray<string>): Promise<ReadonlyArray<{ readonly obj: T, readonly id: string }>>;
-    delete(tenant: string, id: string): Promise<void>;
-    // TODO create
-    // TODO update
-}
-
-/**
  * Repository for a collection
  */
 export interface Repository<T> {
