79f1a7c761774284f16532974e5e3488454d6f48
Thomas Toye <thomas@toye.io>
2019-11-30 17:45:48 +0100

    feat: make properties optional on IUser

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,6 +1,6 @@
 export interface IUser {
-    readonly acceptedPrivacyPolicy: Date;
-    readonly acceptedTermsAndConditions: Date;
+    readonly acceptedPrivacyPolicy?: Date;
+    readonly acceptedTermsAndConditions?: Date;
     readonly displayName: string;
     readonly email: string;
 }
