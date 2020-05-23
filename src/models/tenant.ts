f05098b27708df235371f006cc1a0cfc2158a3f9
Thomas Toye <thomas@toye.io>
2020-05-23 19:08:18 +0200

    Revert "feat: add onlineRegistrationsGoToWaitingList"
    This reverts commit efcc3db9e3203e1cb5f26659aa3478d73c3371bb.

--- src/models/tenant.ts
+++ src/models/tenant.ts
@@ -54,7 +54,6 @@ export interface Tenant {
 
     readonly enableOnlineRegistration?: boolean;
     readonly enableOnlineEnrollment?: boolean;
-    readonly onlineRegistrationsGoToWaitingList?: boolean;
 }
 
 export type ITenant = Tenant;
